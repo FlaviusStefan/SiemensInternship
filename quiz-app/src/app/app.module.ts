ed619a0f668437487f867f30f701f7aba8234a5a
FlaviusStefan <flaviusstefanungureanu@gmail.com>
2023-12-17 04:17:04 +0200

    got rid of unnecessary component

--- quiz-app/src/app/app.module.ts
+++ quiz-app/src/app/app.module.ts
@@ -5,13 +5,12 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuizComponent } from './components/quiz/quiz.component';
-import { ScoreComponent } from './components/score/score.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuizComponent,
-    ScoreComponent,
+    QuizComponent
+
   ],
   imports: [
     BrowserModule,
