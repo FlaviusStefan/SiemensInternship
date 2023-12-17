b15ae5f9a2195d7ccf79a60109b546528138a3a0
FlaviusStefan <flaviusstefanungureanu@gmail.com>
2023-12-17 02:29:10 +0200

    angular + components

---
+++ quiz-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QuizComponent } from './components/quiz/quiz.component';
+import { ScoreComponent } from './components/score/score/score.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuizComponent,
+    ScoreComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
